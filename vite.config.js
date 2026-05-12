11dfc61fdd2e2f894ded073d738bfa84b615b36d
ProFrame <ahmedal3mary@gmail.com>
2026-05-12 15:29:39 +0300

    ت4

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/ShalfaGate/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-v2.js`,
+        chunkFileNames: `assets/[name]-[hash]-v2.js`,
+        assetFileNames: `assets/[name]-[hash]-v2.[ext]`,
+      },
+    },
+  },
 })
